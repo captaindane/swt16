f351bd4acbf10e3a03efc1e989e0c316fd55f7a4
Daniel.MacDane <mighty.mac.gee@gmail.com>
2019-11-19 17:25:58 +0100

    decoder: bugfix for multi-cycle instruction.

--- rtl/decoder.v
+++ rtl/decoder.v
@@ -38,53 +38,54 @@ module decoder #(parameter OPCODE_WIDTH    =  4,
     localparam [OPCODE_WIDTH-1:0] OPCODE_S_TYPE = 4'b0011;
     localparam [OPCODE_WIDTH-1:0] OPCODE_LH     = 4'b0100; // I-TYPE
 
+    // S-TYPE
     localparam [ FUNC1_WIDTH-1:0] FUNC1_JRZ     = 4'b0000;
     localparam [ FUNC1_WIDTH-1:0] FUNC1_JRNZ    = 4'b0001;
     localparam [ FUNC1_WIDTH-1:0] FUNC1_SH      = 4'b0010;
     
+    // U-TYPE
     localparam [ FUNC2_WIDTH-1:0] FUNC2_J       = 4'b0000;
     localparam [ FUNC2_WIDTH-1:0] FUNC2_JAL     = 4'b0001;
     localparam [ FUNC2_WIDTH-1:0] FUNC2_JR      = 4'b0010;
     localparam [ FUNC2_WIDTH-1:0] FUNC2_LI      = 4'b0011;
     localparam [ FUNC2_WIDTH-1:0] FUNC2_LIL     = 4'b0100;
     
-    reg  [PMEM_WORD_WIDTH-1:0]  instr_1st_word_sampled;
-    reg  [PMEM_WORD_WIDTH-1:0]  instr_1st_word;
-    reg  [PMEM_WORD_WIDTH-1:0]  instr_sampled;
+    wire [PMEM_WORD_WIDTH-1:0]  instr_1st_word;  // Holds first word of instruction (also for multi-cycle instructions)
+    reg  [PMEM_WORD_WIDTH-1:0]  instr_sampled;   // Current instruction word
+    reg  [PMEM_WORD_WIDTH-1:0]  instr_sampled2;  // Instruction word before current word instruction
 
     reg  [       PC_WIDTH-1:0]  pc_sampled;
     reg  [       PC_WIDTH-1:0]  pc_sampled2;
 
     reg  [PMEM_ADDR_WIDTH-1:0]  jump_offset;
-    reg                         is_2cycle_instr;
 
     reg  [                2:0]  cycle_in_instr_next;
     reg  [                2:0]  cycle_in_instr_sampled;
 
     
     // R-Type instruction segments
-    wire [   OPCODE_WIDTH-1:0]  opcode       = instr_1st_word_sampled [OPCODE_WIDTH-1:0];
-    wire [  REG_IDX_WIDTH-1:0]  src1_reg_idx = instr_1st_word_sampled [11:8];
-    wire [  REG_IDX_WIDTH-1:0]  src2_reg_idx = instr_1st_word_sampled [15:12];
+    wire [   OPCODE_WIDTH-1:0]  opcode       = instr_1st_word [OPCODE_WIDTH-1:0];
+    wire [  REG_IDX_WIDTH-1:0]  src1_reg_idx = instr_1st_word [11:8];
+    wire [  REG_IDX_WIDTH-1:0]  src2_reg_idx = instr_1st_word [15:12];
 
     assign out_src1_reg_idx = src1_reg_idx; // TODO: null me when i am not needed
     assign out_src2_reg_idx = src2_reg_idx; // TODO: null me when i am not needed
     
     
     // I-Type instruction segments
-    wire [    FUNC2_WIDTH-1:0]  func1 = instr_1st_word_sampled[7:4];
+    wire [    FUNC2_WIDTH-1:0]  func1 = instr_1st_word[7:4];
     
     
     // U-Type instruction segments
-    wire [                3:0]  immA  = instr_1st_word_sampled[15:12];
+    wire [                3:0]  immA  = instr_1st_word[15:12];
     wire [PMEM_WORD_WIDTH-1:0]  immB  = instr_sampled; // only valid in 2nd cycle of multi-cycle instruction
-    wire [    FUNC2_WIDTH-1:0]  func2 = instr_1st_word_sampled[11:8];
+    wire [    FUNC2_WIDTH-1:0]  func2 = instr_1st_word[11:8];
     
     
     // Connecting signals to output ports
     assign out_instr        = instr_sampled;
     assign out_pc           = pc_sampled;
-    assign out_res_reg_idx  = instr_1st_word_sampled[7:4];
+    assign out_res_reg_idx  = instr_1st_word[7:4];
     
 
     // Register: index of the current cycle within a multi-cycle instruction
@@ -96,22 +97,17 @@ module decoder #(parameter OPCODE_WIDTH    =  4,
             cycle_in_instr_sampled <= 0;
     end
 
-    // Register: holds the first word of a multi-cycle instruction
+    // Register: holds the current and last sampled instruction
     always @(posedge clock or posedge reset)
     begin
-        if (!reset)
-            instr_1st_word_sampled <= instr_1st_word;
-        else
-            instr_1st_word_sampled <= 0;
-    end
-
-    // Register: holds the latest instruction
-    always @(posedge clock or posedge reset)
-    begin
-        if (!reset)
-            instr_sampled <= in_instr;
-        else
-            instr_sampled <= 0;
+        if (!reset) begin
+            instr_sampled  <= in_instr;
+            instr_sampled2 <= instr_sampled;
+        end
+        else begin
+            instr_sampled  <= 0;
+            instr_sampled2 <= 0;
+        end
     end
 
     // Register: sample PC
@@ -127,15 +123,8 @@ module decoder #(parameter OPCODE_WIDTH    =  4,
         end
     end
 
-    
     // Multiplexer: Hold first instruction word in case of 2-cycle instruction
-    always @(*)
-    begin
-        if (is_2cycle_instr)
-            instr_1st_word = instr_sampled;
-        else
-            instr_1st_word = in_instr;
-    end
+    assign instr_1st_word = (cycle_in_instr_sampled == 0) ? instr_sampled : instr_sampled2;
 
     // Helper function: set all output to zero
     task zero_outputs;
@@ -150,12 +139,11 @@ module decoder #(parameter OPCODE_WIDTH    =  4,
         out_src2                     = 0;
     endtask;
 
-    // Decode 1st instruction word
+    // Decode instruction word
     always @(*)
     begin
         // Flush
         if (in_flush) begin
-            is_2cycle_instr          = 0;
             cycle_in_instr_next      = 0;
             zero_outputs();
         end
@@ -167,10 +155,8 @@ module decoder #(parameter OPCODE_WIDTH    =  4,
                 // Jump by immediate
                 FUNC2_J:
                 begin
-                    is_2cycle_instr                          = 1;
-                    cycle_in_instr_next                      = 1;
-                    
                     if (cycle_in_instr_sampled == 1) begin
+                        cycle_in_instr_next                  = 0;
                         out_act_ialu_add                     = 1;
                         out_act_incr_pc_is_res               = 0;
                         out_act_jump_to_ialu_res             = 1;
@@ -183,6 +169,7 @@ module decoder #(parameter OPCODE_WIDTH    =  4,
                         out_src2                             = immB;
                     end
                     else begin
+                        cycle_in_instr_next                  = 1;
                         zero_outputs();
                     end
                 end
@@ -190,10 +177,8 @@ module decoder #(parameter OPCODE_WIDTH    =  4,
                 // Jump and link by immediate
                 FUNC2_JAL:
                 begin
-                    is_2cycle_instr                          = 1;
-                    cycle_in_instr_next                      = 1;
-                    
                     if (cycle_in_instr_sampled == 1) begin
+                        cycle_in_instr_next                  = 0;
                         out_act_ialu_add                     = 1;
                         out_act_incr_pc_is_res               = 1;
                         out_act_jump_to_ialu_res             = 1;
@@ -206,14 +191,35 @@ module decoder #(parameter OPCODE_WIDTH    =  4,
                         out_src2                             = immB;
                     end
                     else begin
+                        cycle_in_instr_next                  = 1;
                         zero_outputs();
                     end
                 end
 
+                // Load 16-bit immediate value
+                FUNC2_LI:
+                begin
+                    if (cycle_in_instr_sampled == 1) begin
+                        cycle_in_instr_next                  = 0;
+                        out_act_ialu_add                     = 0;
+                        out_act_incr_pc_is_res               = 0;
+                        out_act_jump_to_ialu_res             = 0;
+                        out_act_load_dmem                    = 0;
+                        out_act_store_dmem                   = 0;
+                        out_act_write_res_to_reg             = 1;
+                        out_act_write_src2_to_res            = 1;
+                        out_src1                             = 0;
+                        out_src2                             = immB;
+                    end
+                    else begin
+                        cycle_in_instr_next                  = 1;
+                        zero_outputs();
+                    end
+                end
+                
                 // Load immediate value to 4 LSBs (no sign extension)
                 FUNC2_LIL:
                 begin
-                    is_2cycle_instr                          = 0;
                     cycle_in_instr_next                      = 0;
                     
                     out_act_ialu_add                         = 0;
@@ -231,11 +237,10 @@ module decoder #(parameter OPCODE_WIDTH    =  4,
                 //
                 default:
                 begin
-                    is_2cycle_instr          = 0;
-                    cycle_in_instr_next      = 0;
+                    cycle_in_instr_next                      = 0;
                     zero_outputs();
 
-                    $display("WARNING: unknown U-Type instruction with opcode %b, func2 %b\n", opcode, func2);
+                    $display("WARNING: unknown U-Type instruction with opcode %b, func2 %b at time %0t.\n", opcode, func2, $time);
                 end
             endcase
         end  // else if (opcode == OPCODE_U_TYPE)
@@ -248,7 +253,6 @@ module decoder #(parameter OPCODE_WIDTH    =  4,
                 // Store half
                 FUNC1_SH:
                 begin
-                    is_2cycle_instr           = 0;
                     cycle_in_instr_next       = 0;
 
                     out_act_ialu_add          = 0;
@@ -264,11 +268,10 @@ module decoder #(parameter OPCODE_WIDTH    =  4,
 
                 default:
                 begin
-                    is_2cycle_instr           = 0;
                     cycle_in_instr_next       = 0;
                     zero_outputs();
 
-                    $display("WARNING: unknown S-Type instruction with opcode %b, func1 %b\n", opcode, func1);
+                    $display("WARNING: unknown S-Type instruction with opcode %b, func1 %b at time %0t.\n", opcode, func1, $time);
                 end
                 
             endcase
@@ -276,9 +279,7 @@ module decoder #(parameter OPCODE_WIDTH    =  4,
     
         // Load from DMEM
         else if (opcode == OPCODE_LH) begin
-            is_2cycle_instr     = 0;
-            cycle_in_instr_next = 0;
-
+            cycle_in_instr_next       = 0;
             out_act_ialu_add          = 0;
             out_act_incr_pc_is_res    = 0;
             out_act_jump_to_ialu_res  = 0;
@@ -292,18 +293,16 @@ module decoder #(parameter OPCODE_WIDTH    =  4,
         
         // No operation
         else if (opcode == OPCODE_NOP) begin
-            is_2cycle_instr     = 0;
-            cycle_in_instr_next = 0;
+            cycle_in_instr_next       = 0;
             zero_outputs();
         end
         
         // default
         else begin
-            is_2cycle_instr     = 0;
-            cycle_in_instr_next = 0;
+            cycle_in_instr_next       = 0;
             zero_outputs();
             
-            $display("WARNING: unknown R-Type instruction with opcode %b\n", opcode);
+            $display("WARNING: unknown R-Type instruction with opcode %b at time %0t.\n", opcode, $time);
         end
     end
 
