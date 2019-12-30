9fd9ae90f904db721d2400d95d7b2a5363f47aa7
Daniel.MacDane <mighty.mac.gee@gmail.com>
2019-12-30 01:25:11 +0100

    adding opcodes for inc, inci

--- rtl/opcodes.v
+++ rtl/opcodes.v
@@ -6,8 +6,19 @@
 `define OPCODE_U_TYPE  4'b0001 
 `define OPCODE_J_TYPE  4'b0010 
 `define OPCODE_S_TYPE  4'b0011 
+
+// I-TYPE
+// +-------+-------+-------+-------+
+// |  immA |  rs1  |   rd  |  opc  |
+// +-------+-------+-------+-------+
+// TODO: convert to J-TYPE and delete
 `define OPCODE_LH      4'b0100  // I-Type: Load half
 `define OPCODE_LHO     4'b0101  // I-Type: Load half with offset
+
+// R-TYPE
+// +-------+-------+-------+-------+
+// |  rs2  |  rs1  |   rd  |  opc  |
+// +-------+-------+-------+-------+
 `define OPCODE_ADD     4'b0110  // R-Type: integer addition
 `define OPCODE_SUB     4'b0111  // R-Type: integer subtraction
 `define OPCODE_MUL     4'b1000  // R-Type: integer multiplication
@@ -19,6 +30,11 @@
 `define OPCODE_XOR     4'b1110  // R-Type: logical xor
 
 // S-TYPE
+// +-------+-------+-------+-------+
+// |  rs2  |  rs1  | func1 |  opc  |
+// +-------+-------+-------+-------+
+// |        immB (optional)        |
+// +-------+-------+-------+-------+
 `define FUNC1_BEQ      4'b0000  // Branch if equal
 `define FUNC1_BNEQ     4'b0001  // Branch if not equal
 `define FUNC1_BGE      4'b0010  // Branch if greater or equal
@@ -27,12 +43,24 @@
 `define FUNC1_SHO      4'b0101  // Store half (i.e., 16-bit) with offset
 
 // U-TYPE
+// +-------+-------+-------+-------+
+// |  immA | func2 |   rd  |  opc  |
+// +-------+-------+-------+-------+
+// |        immB (optional)        |
+// +-------+-------+-------+-------+
+`define FUNC2_INC      4'b0001  // Increment result by 16-bit immediate value
+`define FUNC2_INCL     4'b0010  // Increment result by sign-extended 4-bit immediate value
 `define FUNC2_LI       4'b0011  // Load immediate value
 `define FUNC2_LIL      4'b0100  // Load LSBs from 4-bit immedate value
 
 // J-Type
-`define FUNC2_JAL      4'b0000  // Jump and link
-`define FUNC2_JALR     4'b0001  // Jump and link with address from register
+// +-------+-------+-------+-------+
+// | func3 |  rs1  |   rd  |  opc  |
+// +-------+-------+-------+-------+
+// |        immB (optional)        |
+// +-------+-------+-------+-------+
+`define FUNC3_JAL      4'b0000  // Jump and link
+`define FUNC3_JALR     4'b0001  // Jump and link with address from register
 
 // Special instructions
 `define INSTR_NOP      {{12{1'b0}}, `OPCODE_NOP}
