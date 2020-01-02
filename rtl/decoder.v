b5b5291efe482d85985686f5c0cf472476b9f1d2
Daniel.MacDane <mighty.mac.gee@gmail.com>
2020-01-03 00:42:23 +0100

    converting I-type instructions to J-type instructions to free up space for root opcodes.

--- rtl/decoder.v
+++ rtl/decoder.v
@@ -250,9 +250,7 @@ module decoder #(parameter OPCODE_WIDTH    =  4,
                 src2_used = 1;
             end
 
-            `OPCODE_J_TYPE,
-            `OPCODE_LH,
-            `OPCODE_LHO:
+            `OPCODE_J_TYPE:
             begin
                 src1_used = 1;
                 src2_used = 0;
@@ -748,6 +746,72 @@ module decoder #(parameter OPCODE_WIDTH    =  4,
                         out_src3                               = 0;
                 end
 
+                // Load half word from address in register
+                `FUNC3_LH:
+                begin
+                        cycle_in_instr_next                    = 0;
+                        out_act_branch_ialu_res_ff_eq0         = 0;
+                        out_act_branch_ialu_res_ff_gt0         = 0;
+                        out_act_branch_ialu_res_ff_lt0         = 0;
+                        out_act_ex_incr_pc_is_res              = 0;
+                        out_act_ialu_add                       = 0;
+                        out_act_ialu_and                       = 0;
+                        out_act_ialu_mul                       = 0;
+                        out_act_ialu_neg_src2                  = 0;
+                        out_act_ialu_or                        = 0;
+                        out_act_ialu_sll                       = 0; 
+                        out_act_ialu_sra                       = 0; 
+                        out_act_ialu_src2_is_res               = 1;
+                        out_act_ialu_srl                       = 0; 
+                        out_act_ialu_xor                       = 0;
+                        out_act_jump_to_ialu_res               = 0;
+                        out_act_load_dmem                      = 1;
+                        out_act_store_dmem                     = 0;
+                        out_act_write_res_to_reg               = 1;
+                        out_res_valid_EX                       = 0;             // result cannot be bypassed from EX
+                        out_res_valid_MEM                      = 1;             // result can be bypassed from MEM
+                        out_src1                               = 0;
+                        out_src2                               = src1_mod;      // address (yes, strange to write src1 to src2, but srcX_to_res only exists for src2)
+                        out_src3                               = 0;
+                end
+    
+                // Load half word from address in register plus offset to dmem address
+                `FUNC3_LHO:
+                begin
+                    
+                    if (cycle_in_instr_ff == 1) begin
+                        cycle_in_instr_next                    = 0;
+                        out_act_branch_ialu_res_ff_eq0         = 0;
+                        out_act_branch_ialu_res_ff_gt0         = 0;
+                        out_act_branch_ialu_res_ff_lt0         = 0;
+                        out_act_ex_incr_pc_is_res              = 0;
+                        out_act_ialu_add                       = 1;
+                        out_act_ialu_and                       = 0;
+                        out_act_ialu_mul                       = 0;
+                        out_act_ialu_neg_src2                  = 0;
+                        out_act_ialu_or                        = 0;
+                        out_act_ialu_sll                       = 0; 
+                        out_act_ialu_sra                       = 0; 
+                        out_act_ialu_src2_is_res               = 0;
+                        out_act_ialu_srl                       = 0; 
+                        out_act_ialu_xor                       = 0;
+                        out_act_jump_to_ialu_res               = 0;
+                        out_act_load_dmem                      = 1;
+                        out_act_store_dmem                     = 0;
+                        out_act_write_res_to_reg               = 1;
+                        out_res_valid_EX                       = 0;             // result cannot be bypassed from EX
+                        out_res_valid_MEM                      = 1;             // result can be bypassed from MEM
+                        out_src1                               = src1_mod;
+                        out_src2                               = immB;
+                        out_src3                               = 0;
+                    end
+                    else begin
+                        cycle_in_instr_next                    = 1;
+                        zero_outputs();
+                    end
+
+                end
+
                 default:
                 begin
                         cycle_in_instr_next                    = 0;
@@ -759,71 +823,6 @@ module decoder #(parameter OPCODE_WIDTH    =  4,
             endcase
         end
     
-        // Load half word from DMEM
-        else if (opcode == `OPCODE_LH) begin
-                cycle_in_instr_next                    = 0;
-                out_act_branch_ialu_res_ff_eq0         = 0;
-                out_act_branch_ialu_res_ff_gt0         = 0;
-                out_act_branch_ialu_res_ff_lt0         = 0;
-                out_act_ex_incr_pc_is_res              = 0;
-                out_act_ialu_add                       = 0;
-                out_act_ialu_and                       = 0;
-                out_act_ialu_mul                       = 0;
-                out_act_ialu_neg_src2                  = 0;
-                out_act_ialu_or                        = 0;
-                out_act_ialu_sll                       = 0; 
-                out_act_ialu_sra                       = 0; 
-                out_act_ialu_src2_is_res               = 1;
-                out_act_ialu_srl                       = 0; 
-                out_act_ialu_xor                       = 0;
-                out_act_jump_to_ialu_res               = 0;
-                out_act_load_dmem                      = 1;
-                out_act_store_dmem                     = 0;
-                out_act_write_res_to_reg               = 1;
-                out_res_valid_EX                       = 0;             // result cannot be bypassed from EX
-                out_res_valid_MEM                      = 1;             // result can be bypassed from MEM
-                out_src1                               = 0;
-                out_src2                               = src1_mod;      // address (yes, strange to write src1 to src2, but srcX_to_res only exists for src2)
-                out_src3                               = 0;
-        end
-
-        // Load half word with offset from DMEM
-        else if (opcode == `OPCODE_LHO) begin
-            
-            if (cycle_in_instr_ff == 1) begin
-                cycle_in_instr_next                    = 0;
-                out_act_branch_ialu_res_ff_eq0         = 0;
-                out_act_branch_ialu_res_ff_gt0         = 0;
-                out_act_branch_ialu_res_ff_lt0         = 0;
-                out_act_ex_incr_pc_is_res              = 0;
-                out_act_ialu_add                       = 1;
-                out_act_ialu_and                       = 0;
-                out_act_ialu_mul                       = 0;
-                out_act_ialu_neg_src2                  = 0;
-                out_act_ialu_or                        = 0;
-                out_act_ialu_sll                       = 0; 
-                out_act_ialu_sra                       = 0; 
-                out_act_ialu_src2_is_res               = 0;
-                out_act_ialu_srl                       = 0; 
-                out_act_ialu_xor                       = 0;
-                out_act_jump_to_ialu_res               = 0;
-                out_act_load_dmem                      = 1;
-                out_act_store_dmem                     = 0;
-                out_act_write_res_to_reg               = 1;
-                out_res_valid_EX                       = 0;             // result cannot be bypassed from EX
-                out_res_valid_MEM                      = 1;             // result can be bypassed from MEM
-                out_src1                               = src1_mod;
-                out_src2                               = immB;
-                out_src3                               = 0;
-
-            end
-            else begin
-                cycle_in_instr_next                    = 1;
-                zero_outputs();
-            end
-
-        end
-
         // Integer addition
         else if (opcode == `OPCODE_ADD) begin
                 cycle_in_instr_next                    = 0;
