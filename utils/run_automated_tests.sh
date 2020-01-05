a24fe00c4e7cbb48d062b1eecc197c77b17db112
Daniel.MacDane <mighty.mac.gee@gmail.com>
2020-01-06 00:02:10 +0100

    adding factorial test program to automated tests

--- utils/run_automated_tests.sh
+++ utils/run_automated_tests.sh
@@ -10,11 +10,6 @@ function run_test ()
     DMEM_IN="../prog/${PROG_NAME}.dmem"
     DMEM_OUT="../prog/${PROG_NAME}.dmem.postsim"
     DMEM_GOLD="../prog/${PROG_NAME}.dmem.golden"
-
-    # How many cycles should be run?
-    PIPE_DEPTH=5
-    NUM_CYCLES=$(wc -l ${PMEM} | awk '{print $1}')
-    NUM_CYCLES=$(($NUM_CYCLES + $PIPE_DEPTH))
     
     # Generate simulator command
     SIM="../bench/swt16/Vswt16_top"
@@ -23,7 +18,6 @@ function run_test ()
     # Run simulation
     echo "======================================="
     echo "Test program: ${PROG_NAME}"
-    echo "Test cycles : ${NUM_CYCLES}"
     echo ${SIM_CMD}
     eval ${SIM_CMD}
 
@@ -57,6 +51,7 @@ run_test "hex_test_arith";         NUM_FAILED=$(($NUM_FAILED + $?))
 run_test "hex_test_branch";        NUM_FAILED=$(($NUM_FAILED + $?))
 run_test "hex_test_jmp";           NUM_FAILED=$(($NUM_FAILED + $?))
 run_test "hex_test_load_store";    NUM_FAILED=$(($NUM_FAILED + $?))
+run_test "hex_test_factorial";     NUM_FAILED=$(($NUM_FAILED + $?))
 
 echo "======================================="
 
