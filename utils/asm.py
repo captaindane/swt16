af61944c8df7c54c4275b28c1273590f5fa3c73f
Daniel.MacDane <mighty.mac.gee@gmail.com>
2020-01-18 23:57:26 +0100

    adding S, J, U type instructions to assembler. still not fully functional.

--- utils/asm.py
+++ utils/asm.py
@@ -9,21 +9,21 @@ def parse_isa(root, instr_list, depth, root_opc="0000"):
             
             # Root instruction
             if depth == 0:
-                instr_list.append ( { "mnemonic": child.attrib["name"], "opc": child.attrib["opc"], "Type": "R" } )
+                instr_list.append ( { "mnemonic": child.attrib["name"], "opc": child.attrib["opc"], "cycles": child.attrib["cycles"], "Type": "R" } )
             
             # Hierarchical instruction
             else:
                 # S-Type, which has func1 attribute
                 if "func1" in child.attrib:
-                    instr_list.append ( { "mnemonic": child.attrib["name"], "opc": root_opc, "func1": child.attrib["func1"], "Type": "S" } )
+                    instr_list.append ( { "mnemonic": child.attrib["name"], "opc": root_opc, "cycles": child.attrib["cycles"], "func1": child.attrib["func1"], "Type": "S" } )
 
                 # U-Type, which has func2 attribute
                 elif "func2" in child.attrib:
-                    instr_list.append ( { "mnemonic": child.attrib["name"], "opc": root_opc, "func2": child.attrib["func2"], "Type": "U" } )
+                    instr_list.append ( { "mnemonic": child.attrib["name"], "opc": root_opc, "cycles": child.attrib["cycles"], "func2": child.attrib["func2"], "Type": "U" } )
 
                 # J-Type, which has func3 attribute
                 elif "func3" in child.attrib:
-                    instr_list.append ( { "mnemonic": child.attrib["name"], "opc": root_opc, "func3": child.attrib["func3"], "Type": "J" } )
+                    instr_list.append ( { "mnemonic": child.attrib["name"], "opc": root_opc, "cycles": child.attrib["cycles"], "func3": child.attrib["func3"], "Type": "J" } )
 
         # Recursive call if we are in root of hierarchical instruction
         elif child.tag == "root":
@@ -60,6 +60,37 @@ def strip_asm ( filename ):
     return asm_list;
 
 
+# Function: Parse immediate value. Input format is hex, binary, or decimal. Output format is hex.
+def parse_imm ( str_value, imm_type ):
+
+    num_hex_digits = 0;
+    parsed = "z"
+    
+    # Size of immediate field in instruction in units of hex digits (i.e., 4 bits)
+    if (imm_type == "A"):
+        num_hex_digits = 1
+    elif (imm_type == "B"):
+        num_hex_digits = 4
+
+    num_bits = num_hex_digits*4
+
+    # Input is hex
+    if ( str_value[0:2] == "0x"):
+        parsed = str_value[2:].zfill(num_hex_digits)
+        
+    
+    # Input is binary
+    elif ( str_value[0:2] == "0b"):
+        parsed = hex(int(str_value[2:], 2))[2:].zfill(num_hex_digits)
+
+    # Input is decimal
+    else:
+        int_val = int(str_value, 10)
+        parsed  = hex((int_val + (1<<num_bits)) % (1 << num_bits))[2:].zfill(num_hex_digits)
+
+    return parsed
+
+
 # Function: Generate binary
 def gen_binary ( lines, instr_list ):
 
@@ -82,11 +113,66 @@ def gen_binary ( lines, instr_list ):
                 field3 = ""
                 immB   = ""
 
+                print "DBG: now interpreting line: " + line
+                
+                # R-Type
+                # +=======+=======+=======+=======+
+                # |  rs2  |  rs1  |   rd  |  opc  |
+                # +=======+=======+=======+=======+
                 if (instr_desc["Type"] == "R"):
 
                     field1 = hex(int(elem_list[1][1:]))[2:]
                     field2 = hex(int(elem_list[2][1:]))[2:]
                     field3 = hex(int(elem_list[3][1:]))[2:]
+
+                    print line + " -> " + field3 + field2 + field1 + opc
+
+                # S-Type
+                # +=======+=======+=======+=======+
+                # |  rs2  |  rs1  | func1 |  opc  |
+                # +=======+=======+=======+=======+
+                # |        immB (optional)        |
+                # +===============================+
+                elif (instr_desc["Type"] == "S"):
+                    
+                    field1 = hex(int(instr_desc["func1"], 2))[2:]
+                    field2 = hex(int(elem_list[1][1:]))[2:]
+                    field3 = hex(int(elem_list[2][1:]))[2:]
+                    
+                    print line + " -> " + field3 + field2 + field1 + opc
+
+
+                # U-Type
+                # +=======+=======+=======+=======+
+                # |  immA | func2 |   rd  |  opc  |
+                # +=======+=======+=======+=======+
+                # |        immB (optional)        |
+                # +===============================+
+                elif (instr_desc["Type"] == "U"):
+                    
+                    field1 = hex(int(elem_list[1][1:]))[2:]
+                    field2 = hex(int(instr_desc["func2"], 2))[2:]
+
+                    if (instr_desc["cycles"] == "1"):
+                        field3 = parse_imm(elem_list[2], "A")
+                        print line + " -> " + field3 + field2 + field1 + opc
+
+                    elif (instr_desc["cycles"] == "2"):
+                        field3 = "0"
+                        field4 = parse_imm(elem_list[2], "B")
+                        print line + " -> " + field3 + field2 + field1 + opc + " , " + field4
+
+                # J-Type
+                # +=======+=======+=======+=======+
+                # | func3 |  rs1  |   rd  |  opc  |
+                # +=======+=======+=======+=======+
+                # |        immB (optional)        |
+                # +===============================+
+                elif (instr_desc["Type"] == "J"):
+                    
+                    field1 = hex(int(elem_list[1][1:]))[2:]
+                    field2 = hex(int(elem_list[2][1:]))[2:]
+                    field3 = hex(int(instr_desc["func3"], 2))[2:]
 
                     print line + " -> " + field3 + field2 + field1 + opc
 
