2e1277b9dc544f3aed0ff5bffc125c80222e4aad
Daniel.MacDane <mighty.mac.gee@gmail.com>
2020-01-31 01:07:27 +0100

    naming of test programs

--- utils/run_automated_tests.sh
+++ utils/run_automated_tests.sh
@@ -53,12 +53,12 @@ fi
 
 # Runt tests
 NUM_FAILED=0
-run_test "hex_test_bypass_stall";  NUM_FAILED=$(($NUM_FAILED + $?))
-run_test "hex_test_arith";         NUM_FAILED=$(($NUM_FAILED + $?))
-run_test "hex_test_branch";        NUM_FAILED=$(($NUM_FAILED + $?))
-run_test "hex_test_jmp";           NUM_FAILED=$(($NUM_FAILED + $?))
-run_test "hex_test_load_store";    NUM_FAILED=$(($NUM_FAILED + $?))
-run_test "hex_test_factorial";     NUM_FAILED=$(($NUM_FAILED + $?))
+run_test "test_bypass_stall";  NUM_FAILED=$(($NUM_FAILED + $?))
+run_test "test_arith";         NUM_FAILED=$(($NUM_FAILED + $?))
+run_test "test_branch";        NUM_FAILED=$(($NUM_FAILED + $?))
+run_test "test_jmp";           NUM_FAILED=$(($NUM_FAILED + $?))
+run_test "test_load_store";    NUM_FAILED=$(($NUM_FAILED + $?))
+run_test "test_factorial";     NUM_FAILED=$(($NUM_FAILED + $?))
 
 echo "======================================="
 
